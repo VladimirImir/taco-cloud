98c87584abff59d6eaa3dece2fc77c4a5d29a0da
VladimirImir <mimimvvv@gmail.com>
2023-05-22 20:41:16 +0300

    9 - commit

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -4,22 +4,22 @@ delete from Taco_Order;
 
 delete from Ingredient;
 insert into Ingredient (id, name, type)
-                values ('FLTO', 'Мучная Тортилья', 'WRAP');
+                values ('1', 'Мучная Тортилья', 'WRAP');
 insert into Ingredient (id, name, type)
-                values ('COTO', 'Кукурузная Тортилья', 'WRAP');
+                values ('2', 'Кукурузная Тортилья', 'WRAP');
 insert into Ingredient (id, name, type)
-                values ('GRBF', 'Говяжий фарш', 'PROTEIN');
+                values ('3', 'Говяжий фарш', 'PROTEIN');
 insert into Ingredient (id, name, type)
-                values ('CARN', 'Карнитас', 'PROTEIN');
+                values ('4', 'Карнитас', 'PROTEIN');
 insert into Ingredient (id, name, type)
-                values ('TMTO', 'Нарезанные помидоры', 'VEGGIES');
+                values ('5', 'Нарезанные помидоры', 'VEGGIES');
 insert into Ingredient (id, name, type)
-                values ('LETC', 'Салат', 'VEGGIES');
+                values ('6', 'Салат', 'VEGGIES');
 insert into Ingredient (id, name, type)
-                values ('CHED', 'Чеддер', 'CHEESE');
+                values ('7', 'Чеддер', 'CHEESE');
 insert into Ingredient (id, name, type)
-                values ('JACK', 'Монтерей джек', 'CHEESE');
+                values ('8', 'Монтерей джек', 'CHEESE');
 insert into Ingredient (id, name, type)
-                values ('SLSA', 'Сальса', 'SAUCE');
+                values ('9', 'Сальса', 'SAUCE');
 insert into Ingredient (id, name, type)
-                values ('SRCR', 'Сметана', 'SAUCE');
+                values ('10', 'Сметана', 'SAUCE');
